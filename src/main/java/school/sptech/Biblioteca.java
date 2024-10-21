4425f540c311bf3d31de10d29d9c503c3c4532b4
miguelluvizon <miguel.luvizon@sptech.school>
2024-10-20 22:38:06 -0300

    feito

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,121 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.LivroNaoEncontradoException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+    private String nome;
+    private List<Livro> livros = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public void adicionarLivro(Livro livro){
+        if (livro == null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (livro.getTitulo()== null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (livro.getTitulo().isBlank()){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (livro.getAutor()== null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (livro.getAutor().isBlank()){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (livro.getDataPublicacao() == null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+
+        livros.add(livro);
+    }
+
+    public void removerLivroPorTitulo(String titulo){
+        if (titulo == null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (titulo.isEmpty()) {
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (titulo.equals(" ")){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+
+        Integer encontrei = 0;
+
+        for (int i = 0; i < livros.size(); i++) {
+            if(titulo.equalsIgnoreCase(livros.get(i).getTitulo())){
+                encontrei = 1;
+                livros.remove(livros.get(i));
+            }
+        }
+        if (encontrei == 0){
+            throw new LivroNaoEncontradoException("nao encontrado");
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo){
+        if (titulo == null){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (titulo.isEmpty()) {
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+        if (titulo.equals(" ")){
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+
+        Integer encontrei = 0;
+        Livro livroCerto = null;
+
+        for (int i = 0; i < livros.size(); i++) {
+            if(titulo.equalsIgnoreCase(livros.get(i).getTitulo())){
+                encontrei = 1;
+                livroCerto = livros.get(i);
+            }
+        }
+        if (encontrei == 0){
+            throw new LivroNaoEncontradoException("nao encontrado");
+        }
+        return livroCerto;
+    }
+
+    public Integer contarLivros(){
+        Integer total = 0;
+        for (int i = 0; i < livros.size(); i++) {
+            total = total + 1;
+        }
+        return total;
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano){
+        List<Livro> livrosAno = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (livro.getDataPublicacao().getYear() <= ano) {
+                livrosAno.add(livro);
+            }
+        }
+        return livrosAno;
+    }
+
 
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,94 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes = new ArrayList<>();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public Livro() {
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", avaliacaos=" + avaliacoes +
+                '}';
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas) {
+        if (descricao.isBlank()) {
+            throw new ArgumentoInvalidoException("Argumento inválido");
+        }
+        if (descricao == null) {
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+
+        if (qtdEstrelas < 0.0 || qtdEstrelas > 5.0) {
+            throw new ArgumentoInvalidoException("Argumento invalido");
+        }
+
+        Avaliacao adicionar = new Avaliacao();
+        adicionar.setDescricao(descricao);
+        adicionar.setQtdEstrelas(qtdEstrelas);
+        avaliacoes.add(adicionar);
+    }
+
+    public Double calcularMediaAvaliacoes(){
+        if (avaliacoes.isEmpty()) {
+            return 0.0;
+        }
+        Double media = 0.0;
+        Double total = 0.0;
+
+        for (Avaliacao avaliacao : avaliacoes) {
+            total = total + avaliacao.getQtdEstrelas();
+        }
+        media = total/ avaliacoes.size();
+        return media;
+    }
+
+
 
 }
